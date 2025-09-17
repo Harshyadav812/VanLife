2415bb82975758422fbd0d43ef74df04ae0bd3ee
Harshyadav812 <harshrao812@gmail.com>
2025-09-17 18:07:47 +0000

    Refactors route component structure

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,25 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 
-import MainLayout from '../layouts/MainLayout'
+import MainLayout from "../layouts/MainLayout";
 
 export const Route = createRootRoute({
   component: () => (
-    
-      <MainLayout>
-        <Outlet />
-      </MainLayout>
-
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
   ),
-  errorComponent: ({ error }) => <div>Something went wrong: {error.message}</div>
-})
+  errorComponent: ({ error }) => (
+    <div>Something went wrong: {error.message}</div>
+  ),
+
+  notFoundComponent: NotFoundComponent,
+});
+
+function NotFoundComponent() {
+  return (
+    <div className="w-full flex flex-col justify-center items-center font-extrabold text-2xl">
+      <h2>Oops..!</h2>
+      <p>The Page you're looking for was not found</p>
+    </div>
+  );
+}
