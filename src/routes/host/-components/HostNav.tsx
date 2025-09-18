01c44fe0d011788899d772d06f2412849db2c127
Harshyadav812 <harshrao812@gmail.com>
2025-09-18 12:47:08 +0000

    Refactors HostNav to use relative paths

--- src/routes/host/-components/HostNav.tsx
+++ src/routes/host/-components/HostNav.tsx
@@ -1,17 +1,19 @@
 import { Link } from "@tanstack/react-router";
+import { Route as hostRoute } from "../route";
 
 export function HostNav() {
   const links = [
-    { to: "/host", label: "Dashboard", activeOptions: { exact: true } },
-    { to: "/host/income", label: "Income" },
-    { to: "/host/vans", label: "Vans", activeOptions: { exact: true } },
-    { to: "/host/reviews", label: "Reviews" },
+    { to: ".", label: "Dashboard", activeOptions: { exact: true } },
+    { to: "income", label: "Income" },
+    { to: "vans", label: "Vans", activeOptions: { exact: true } },
+    { to: "reviews", label: "Reviews" },
   ];
 
   return (
     <nav className="flex space-x-6 border-b border-gray-200 pb-2 mb-4">
       {links.map((option) => (
         <Link
+          from={hostRoute.fullPath}
           {...option}
           key={option.to}
           className="text-gray-600 hover:text-slate-900 hover:font-bold hover:underline transition-colors"
