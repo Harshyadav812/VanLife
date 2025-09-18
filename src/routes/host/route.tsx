66fe4a7d3ef14edf60fc978716b11e034cfa67b2
Harshyadav812 <harshrao812@gmail.com>
2025-09-18 12:49:39 +0000

    Adds loader to fetch host vans

--- src/routes/host/route.tsx
+++ src/routes/host/route.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { HostNav } from "./-components/HostNav";
+import type { Van } from "../vans/index.lazy";
 
 export const Route = createFileRoute("/host")({
+  loader: async () => {
+    const res = await fetch(`/api/host/vans`);
+    const data = await res.json();
+    return { hostVans: data.vans as Van[] };
+  },
   component: HostLayout,
 });
 
