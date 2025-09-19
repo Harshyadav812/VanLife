9cfd93ce972abf3516701931591be174c1349054
Harshyadav812 <harshrao812@gmail.com>
2025-09-19 12:42:08 +0000

    Refactors NotFound component
    Refactors NotFound Component for better UX and adds TanStackRouterDevtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,33 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import MainLayout from "../layouts/MainLayout";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Button from "../components/ui/Button";
 
 export const Route = createRootRoute({
   component: () => (
     <MainLayout>
       <Outlet />
+      <TanStackRouterDevtools />
     </MainLayout>
   ),
   errorComponent: ({ error }) => (
     <div>Something went wrong: {error.message}</div>
   ),
 
-  notFoundComponent: NotFoundComponent,
+  notFoundComponent: NotFoundPage,
 });
 
-function NotFoundComponent() {
+function NotFoundPage() {
   return (
-    <div className="w-full flex flex-col justify-center items-center font-extrabold text-2xl">
-      <h2>Oops..!</h2>
-      <p>The Page you're looking for was not found</p>
+    <div className="w-full flex flex-col justify-center items-center font-extrabold text-2xl px-5 py-5">
+      <p className="font-bold text-4xl mb-4">
+        Sorry, the page you were looking for was not found.
+      </p>
+      <Link to="/">
+        <Button variant="luxury" size="lg" className="!w-full sm:!w-80">
+          Return to Home
+        </Button>
+      </Link>
     </div>
   );
 }
